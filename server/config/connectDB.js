c6c7d086ce7dc84a89b9d0f42517f9f2d6e1931f
GaromaMakure <garomamak11@gmail.com>
2025-03-15 05:25:47 -0700

    updating server folder

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -3,8 +3,16 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("connect to DB");
+    });
+    connection.on("error", (error) => {
+      console.log("something is wrong in mongodb", error);
+    });
   } catch (error) {
     console.log("something is wrong ", error);
   }
 }
+module.exports = connectDB
+
